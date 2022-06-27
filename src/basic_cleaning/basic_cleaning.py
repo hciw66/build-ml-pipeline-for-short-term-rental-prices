2e02ef3e2e882c3ec8fd4c86cb7c925556d17824
hciw66 <hciw66@gmail.com>
2022-06-27 18:21:16 -0400

    add lat/lon condition to basci_cleaning.py

--- src/basic_cleaning/basic_cleaning.py
+++ src/basic_cleaning/basic_cleaning.py
@@ -31,6 +31,9 @@ def go(args):
     df = df[idx].copy()
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'], errors='coerce')
+    #add lat/lon condition
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     #save datafrme as a csv file
     df.to_csv('clean_sample.csv', index=False)
     # put process dato into wandb as artifact
